60a1562b2ed6d509dda19c1fa5979f885409a6f8
Matthew Whited <matt@whited.us>
2025-02-17 16:04:35 -0500

    update examples

--- backend/shell.bat
+++ backend/shell.bat
@@ -1,11 +1,10 @@
 @ECHO OFF
 SETLOCAL
 
-SET TARGET_MACHINE=open-web-ui
 IF NOT "%1"=="" SET TARGET_MACHINE=%1
 
 CALL container-config
 
-CALL docker exec -it --user root %TARGET_MACHINE% bash
+CALL docker exec -it --user root %CONTAINER_GROUP%-%TARGET_MACHINE%-1 bash
 
 ENDLOCAL
